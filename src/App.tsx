eaf60f72658183390d249b2c66f2f778ca5e608e
Ron Almog <ron8132@gmail.com>
2021-11-14 10:39:58 -0500

    add reducer, interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./App.css";
 import NewNoteInput from "./NewNoteInput";
+import { NotesState } from "./store/noteReducer";
 
 function App() {
+  const notes = useSelector<NotesState, NotesState["notes"]>(
+    (state) => state.notes
+  );
   return (
     <>
       <NewNoteInput addNote={alert} />
       <hr />
-      <ul></ul>
+      <ul>
+        {notes.map((note) => {
+          return <li key={note}>{note}</li>;
+        })}
+      </ul>
     </>
   );
 }
---
+++ src/store/noteReducer.ts
@@ -0,0 +1,22 @@
+export interface NotesState {
+  notes: string[];
+}
+const initialState = {
+  notes: [],
+};
+type Action = { type: "ADD_NOTE"; payload: "string" };
+export const noteReducer = (
+  state: NotesState = initialState,
+  action: Action
+) => {
+  switch (action.type) {
+    case "ADD_NOTE":
+      return {
+        ...state,
+        notes: [...state.notes, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
